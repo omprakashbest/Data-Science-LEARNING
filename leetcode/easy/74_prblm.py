1a970592be2b9ff2aed9980ff34cd26d405aa7b8
Om <omprakashbest3@gmail.com>
2026-08-11 22:54:25 +0530

    Leetcode: Problem 1598

---
+++ leetcode/easy/74_prblm.py
@@ -0,0 +1,35 @@
+"""
+-> Crawler Log Folder 
+
+The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+The operations are described below:
+• "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in 
+the same folder).
+• "./" : Remain in the same folder.
+• "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+
+You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+The file system starts in the main folder, then the operations in logs are performed.
+
+Return the minimum number of operations needed to go bak to the main folder after the change folder operations.
+"""
+
+class Solution:
+    def minOperations(self, logs: list[str]) -> int:
+        res = 0
+
+        for log in logs:
+            if log == "./":
+                continue
+            elif log == "../":
+                res = max(0, res - 1)
+            else:
+                res += 1
+        return res
+
+# Example usage:
+obj = Solution()
+logs = ["d1/", "d2/", "../", "d21/", "./"]
+print(obj.minOperations(logs))
